2891e053fa16bfd045aef1fbd48e72b19b137ecb
deproger <vl.ebel.tm@gmail.com>
2023-06-03 16:03:24 +0700

    version without slider

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+try{
+	$('body')
+}
+catch{
+	alert("Oooops! Something go wrong! Please, contact us. Error code: 01 :(")
+}
+const mainSlider = ['1.jpg'];
+
+function prevImage(slider, elem, i) {
+	
+}
