73651cf7557ee3ca53b4c9779a6ea603e0fcea27
deproger <vl.ebel.tm@gmail.com>
2023-06-03 17:34:22 +0700

    version with coposer and sending notifications and meta tags

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 	<link rel="stylesheet" type="text/css" href="scripts/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,800" rel="stylesheet">
 	<title>EbelWEB</title>
-	<meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 	<div id="pc-vers">
